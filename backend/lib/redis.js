e6a3a57318fb4620460e7c6cf9ebc80f82426cd8
DJ <imdarlenequebral@gmail.com>
2025-06-26 18:45:05 +0800

    user and product done

---
+++ backend/lib/redis.js
@@ -0,0 +1,9 @@
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redis = new Redis(process.env.REDIS_URL);
+await redis.set("foo", "bar");
+
+export default redis;
