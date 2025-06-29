be7d71285bec2088e208a33757cb00a986da39a2
DJ <imdarlenequebral@gmail.com>
2025-06-30 00:29:50 +0800

    payment done

--- backend/controllers/payment.contoller.js
+++ backend/controllers/payment.contoller.js
@@ -1,4 +1,6 @@
 import Coupon from "../models/coupon.model.js";
+import stripe from "../lib/stripe.js";
+import Order from "../models/order.model.js";
 
 export const createCheckoutSession = async (req, res) => {
   try {
@@ -41,5 +43,104 @@ export const createCheckoutSession = async (req, res) => {
         );
       }
     }
-  } catch (error) {}
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/purchase-success?session_id=${CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+      discounts: coupon
+        ? [
+            {
+              coupon: await createStripeCoupon(coupon.discountPercentage),
+            },
+          ]
+        : [],
+      metadata: {
+        userId: req.user._id,
+        couponCode: couponCode || "",
+        products: JSON.stringify(
+          products.map((p) => ({
+            id: p._id,
+            quantity: p.quantity,
+            price: p.price,
+          }))
+        ),
+      },
+    });
+
+    if (totalAmount >= 20000) {
+      await createNewCoupon(req.user._id);
+    }
+
+    res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
+  } catch (error) {
+    console.log("Error in createCheckoutSession controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
+
+export const checkoutSucess = async (req, res) => {
+  try {
+    const { sessionId } = req.body;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    if (session.payment_status === "paid") {
+      if (session.metadata.couponCode) {
+        await Coupon.findOneAndUpdate(
+          {
+            code: session.metadata.couponCode,
+            userId: session.metadata.userId,
+          },
+          { isActive: false }
+        );
+      }
+
+      const products = JSON.parse(session.metadata.products);
+      const newOrder = new Order({
+        user: session.metadata.userId,
+        products: products.map((product) => ({
+          product: product.id,
+          quantity: product.quantity,
+          price: product.price,
+        })),
+        totalAmount: session.amount_total / 100,
+        stripeSessionId: sessionId,
+      });
+
+      await newOrder.save();
+      res.status(200).json({
+        success: true,
+        message:
+          "Payment successful, order created, and coupon deactivated if used.",
+        orderId: newOrder._id,
+      });
+    }
+  } catch (error) {
+    console.log("Error in checkoutSucess controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+
+  return coupon.id;
+}
+
+async function createNewCoupon(userId) {
+  const newCoupon = new Coupon({
+    code:
+      "GIFT" + Math.random().toString(36).substring(2, 8).toLocaleUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 100),
+  });
+
+  await newCoupon.save();
+
+  return newCoupon;
+}
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createCheckoutSession } from "../controllers/payment.contoller.js";
+import {
+  checkoutSucess,
+  createCheckoutSession,
+} from "../controllers/payment.contoller.js";
 
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSucess);
 
 export default router;
