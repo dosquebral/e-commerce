f3f073a4cf3bd5a2f10707f80ccf086c145110e3
DJ <imdarlenequebral@gmail.com>
2025-06-29 23:42:03 +0800

    payment

---
+++ backend/controllers/payment.contoller.js
@@ -0,0 +1,45 @@
+import Coupon from "../models/coupon.model.js";
+
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const { products, couponCode } = req.body;
+
+    if (!Array.isArray(products) || products.length === 0) {
+      return res.status(400).json({ error: "Invalid or empty products array" });
+    }
+
+    let totalAmount = 0;
+
+    // products
+    const lineItems = products.map((product) => {
+      const amount = Math.round(product.price * 100); // cents
+      totalAmount += amount * product.quantity;
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: product.name,
+            images: [product.image],
+          },
+          unit_amount: amount,
+        },
+      };
+    });
+
+    let coupon = null;
+
+    if (coupon) {
+      coupon = await Coupon.findOne({
+        code: couponCode,
+        userId: req.user._id,
+        isActive: true,
+      });
+
+      if (coupon) {
+        totalAmount -= Math.round(
+          (totalAmount * coupon.discountPercentage) / 100
+        );
+      }
+    }
+  } catch (error) {}
+};
